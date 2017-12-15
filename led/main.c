2425298e68b1e876a5b733597143911a7b2e4f70
Rajiv Ghoshroy <ghoshroy@gmail.com>
2017-12-15 09:22:31 -0500

    Working version of LED blink

--- led/main.c
+++ led/main.c
@@ -1,24 +1,37 @@
 
 #include "stm32f4_discovery.h"
 
-Led_TypeDef LEDS[] = {LED4, LED3, LED5, LED4, LED6};
+/* LED3: Orange (E) 
+ * LED4: Green  (N)
+ * LED5: Red    (S)
+ * LED6: Blue   (W)
+ */
+//Led_TypeDef LEDS[] = {LED3, LED4, LED3, LED4, LED5, LED4, LED6};
+Led_TypeDef LEDS[] = {LED3, LED4, LED3, LED6, LED4};
+
+void LEDInit(Led_TypeDef led);
 
 int main(void) {
   int idx = 0;
   int nLEDs = sizeof(LEDS) / sizeof(LEDS[0]);
   
-  STM_EVAL_LEDInit(LED4);
-  STM_EVAL_LEDInit(LED3);
-  STM_EVAL_LEDInit(LED5);
-  STM_EVAL_LEDInit(LED6);
+  LEDInit(LED4);
+  LEDInit(LED3);
+  LEDInit(LED5);
+  LEDInit(LED6);
   while (1) {
     int i;
-    for (i = 0; i < 20000; i++) {}
+    for (i = 0; i < 5000000; i++) {}
     STM_EVAL_LEDOff(LEDS[idx]);
     if (++idx == nLEDs) idx = 0;
     STM_EVAL_LEDOn(LEDS[idx]);
   }
   return 0;
+}
+
+void LEDInit(Led_TypeDef led) {
+  STM_EVAL_LEDInit(led);
+  STM_EVAL_LEDOff(led);
 }
 
 
